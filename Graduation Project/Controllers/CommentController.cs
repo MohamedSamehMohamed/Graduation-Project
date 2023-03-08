178bcfa92cd3c27c74996c97e0ad5297390fa05b
MohamedSameh <m.samehmohamed123@gmail.com>
2023-03-08 12:49:19 +0200

    refactor About, Blog, Comment Controllers

--- Graduation Project/Controllers/CommentController.cs
+++ Graduation Project/Controllers/CommentController.cs
@@ -7,95 +7,77 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
-using System.Threading.Tasks;
 
 namespace GraduationProject.Controllers
 {
     public class CommentController : Controller
     {
-        private readonly IRepository<Comment> comments;
-        private readonly IHttpContextAccessor _httpContextAccessor;
-        private User user;
-        private int AllVote;
-        private int BlodId;
-        public CommentController(IRepository<GraduationProject.Data.Models.Comment> comments
-            , IUserRepository<User> Userrepository
+        private readonly IRepository<Comment> _comments;
+        private readonly User _user;
+        private int _allVote;
+        private int _blodId;
+        public CommentController(IRepository<Comment> comments
+            , IUserRepository<User> userRepository
             , IHttpContextAccessor httpContextAccessor
             )
         {
-            this.comments = comments;
-            _httpContextAccessor = httpContextAccessor;
-            var userId = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
-
-            user = Userrepository.Find(userId);
-
+            _comments = comments;
+            var userId = httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            _user = userRepository.Find(userId);
         }
 
         // GET: CommentController
         public ActionResult Index()
         {
             var list = new List<ViewCommentModel>();
-            foreach (var item in comments.List())
-                list.Add(getViewModelFromComment(item));
-
+            foreach (var item in _comments.List())
+                list.Add(_getViewModelFromComment(item));
             return View(list);
         }
-        private ViewCommentModel getViewModelFromComment(GraduationProject.Data.Models.Comment comment)
+        private ViewCommentModel _getViewModelFromComment(Comment comment)
         {
-            
             var commentVote = comment.CommentVotes.FirstOrDefault(b => b.CommentId == comment.CommentId);
-            bool IsOwner = false;
-            if (commentVote.User.UserIdentityId == user.UserIdentityId)
-            {
-                IsOwner = true;
-            }
+            var isOwner = commentVote != null && commentVote.User.UserIdentityId == _user.UserIdentityId;
+            var ownerName = commentVote != null ? commentVote.User.FirstName : "";
             var model = new ViewCommentModel
             {
-               commentId=comment.CommentId,
-               commentOwner= commentVote.User.FirstName,
-               content= comment.Content,
-               creationTime=comment.CreationTime,
-               vote=AllVote,
-               isOwner=IsOwner
+                commentId=comment.CommentId,
+                commentOwner= ownerName,
+                content= comment.Content,
+                creationTime=comment.CreationTime,
+                isOwner=isOwner
             };
             return model;
-        }
-        // GET: CommentController/Details/5
-        public ActionResult Details(int id)
-        {
-            var comment = comments.Find(id);
-            return View(comment);
         }
 
         // GET: CommentController/Create
         public ActionResult Create()
         {
-            int id = (int)TempData["mydata"];
-              var newComment = new Comment{BlogId=id };
+            var id = (int)TempData["mydata"]!;
+            var newComment = new Comment{BlogId=id };
             return View(newComment);
         }
 
         // POST: CommentController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(GraduationProject.Data.Models.Comment model)
+        public ActionResult Create(Comment model)
         {
             try
             {
-                var newComment = new GraduationProject.Data.Models.Comment
+                var newComment = new Comment
                 {
                     Content=model.Content,
                     Upvote=0
                     ,DownVote=0,
-                    CreationTime=DateTime.Now,
                     BlogId=model.BlogId
                 };
-                comments.Add(newComment);
-                int userId = user.UserId;
-                int commentId = newComment.BlogId;
+                _comments.Add(newComment);
+                var userId = _user.UserId;
+                var commentId = newComment.BlogId;
                 var commentVotes = CreateRelation(userId, commentId);
                 newComment.CommentVotes.Add(commentVotes);
-                comments.Update(newComment);
+                _comments.Update(newComment);
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -105,39 +87,39 @@ namespace GraduationProject.Controllers
         }
         private CommentVote CreateRelation(int userId, int commentId)
         {
-            var commentVotes = new CommentVote {
+            var commentVotes = new CommentVote 
+            {
               CommentId=commentId,
               UserId=userId
               ,IsFavourite=false,
               Value=0,
-              User=user
+              User=_user
             };
             return commentVotes;
         }
         // GET: CommentController/Edit/5
         public ActionResult Edit(int id)
         {
-            var comment = comments.Find(id);
+            var comment = _comments.Find(id);
             return View(comment);
         }
 
         // POST: CommentController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id,GraduationProject.Data.Models.Comment model)
+        public ActionResult Edit(int id, Comment model)
         {
             try
             {
-                var newComment = new GraduationProject.Data.Models.Comment
+                var newComment = new Comment
                 {
                     Content = model.Content,
-                    Upvote = 0
-                  ,
+                    Upvote = 0,
                     DownVote = 0,
                     CreationTime = model.CreationTime,
                     BlogId = model.BlogId
                 };
-                comments.Update(newComment);
+                _comments.Update(newComment);
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -149,7 +131,7 @@ namespace GraduationProject.Controllers
         // GET: CommentController/Delete/5
         public ActionResult Delete(int id)
         {
-            var comment = comments.Find(id);
+            var comment = _comments.Find(id);
             return View(comment);
         }
 
@@ -160,7 +142,7 @@ namespace GraduationProject.Controllers
         {
             try
             {
-                comments.Remove(model.CommentId);
+                _comments.Remove(model.CommentId);
                 return RedirectToAction(nameof(Index));
             }
             catch
